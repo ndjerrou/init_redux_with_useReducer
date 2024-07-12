f0cd1095c8b888efa0dcce92f1459e9fbe2af688
ndjerrou <n.djerrou@gmail.com>
2024-07-12 09:32:29 +0100

    added explanation with immer

---
+++ src/components/CounterWithImmer.js
@@ -0,0 +1,93 @@
+import { useReducer, useRef } from 'react';
+import { produce } from 'immer';
+
+function Counter() {
+  const refInput = useRef(null);
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'INCREMENT_BY_ONE': {
+        state.counter++;
+        return;
+        // return {
+        //   counter: state.counter + 1,
+        // };
+      }
+      case 'DECREMENT_BY_ONE': {
+        state.counter--;
+        return;
+        // return {
+        //   counter: state.counter - 1,
+        // };
+      }
+      case 'INCREMENT_A_LOT': {
+        state.counter += action.payload.add;
+        // return {
+        //   counter: state.counter + +action.payload.add,
+        // };
+      }
+      case 'RESET': {
+        state.counter = 0;
+        return {
+          counter: 0,
+        };
+      }
+
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(produce(reducer), {
+    counter: 0,
+  });
+
+  const actionIncrement = {
+    type: 'INCREMENT_BY_ONE',
+  };
+
+  const actionDecrement = {
+    type: 'DECREMENT_BY_ONE',
+  };
+
+  const handleIncrement = () => {
+    dispatch(actionIncrement);
+  };
+
+  const handleDecrement = () => {
+    dispatch(actionDecrement);
+  };
+
+  const handleReset = () => {
+    dispatch({
+      type: 'RESET',
+    });
+  };
+
+  const handleChange = () => {
+    dispatch({
+      type: 'INCREMENT_A_LOT',
+      payload: {
+        add: refInput.current.value,
+      },
+    });
+  };
+
+  //   console.log(state);
+
+  return (
+    <div>
+      Counter
+      <div>{state.counter}</div>
+      <button onClick={handleIncrement}>+1</button>
+      <button onClick={handleDecrement}>-1</button>
+      <button onClick={handleReset}>reset</button>
+      <div>
+        Add by : <input ref={refInput} />
+        <button onClick={handleChange}>Go ! </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
